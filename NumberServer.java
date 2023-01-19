fce2c5e793a0f27a5140e110f89b8ef7501f4b12
lveneUCSD <lvenetoulias@ucsd.edu>
2023-01-19 12:51:18 -0800

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -8,7 +8,7 @@ class Handler implements URLHandler {
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
+            return String.format("Lucas's number: %d", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
@@ -28,7 +28,7 @@ class Handler implements URLHandler {
 
 class NumberServer {
     public static void main(String[] args) throws IOException {
-        if(args.length == 0){
+        if (args.length == 0) {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
